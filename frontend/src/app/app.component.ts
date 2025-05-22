841ff2c0d75bec1f007e9e5cef2ad77e1bfdf37b
JavierCP94 <javiercabreraparra94@gmail.com>
2025-05-22 08:11:04 +0200

    header&footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./modules/footer/footer.component";
+import { HeaderComponent } from "./modules/header/header.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Angular Frontend';
+  title = "Angular Frontend";
 }
