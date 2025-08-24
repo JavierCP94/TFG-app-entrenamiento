6635e27ca56d5fe19908919426b05fc5f3173dfc
Javier Cabrera <javiercabreraparra94@gmail.com>
2025-08-24 13:07:10 +0200

    *

--- backend/src/main/java/com/example/springbackend/controller/SpaForwardController.java
+++ backend/src/main/java/com/example/springbackend/controller/SpaForwardController.java
@@ -2,6 +2,7 @@ package com.example.springbackend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class SpaForwardController {
@@ -14,10 +15,14 @@ public class SpaForwardController {
         "/dashboard",
         "/exercises",
         "/workouts",
-        "/profile",
-        "/**/{path:^[^.]*$"
+        "/profile"
     })
     public String forwardToSpa() {
+        return "forward:/index.html";
+    }
+    
+    @GetMapping("/{path:[^\.]*}")
+    public String redirect(@PathVariable String path) {
         return "forward:/index.html";
     }
 }
